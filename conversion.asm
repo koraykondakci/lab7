720eca42e8f4e1cad7ae959ed2922cc888b608c2
Koray Kondakci <koraykondakci@cs.ucsb.edu>
2023-02-27 03:13:12 -0800

    .

--- conversion.asm
+++ conversion.asm
@@ -4,28 +4,28 @@
 #   make all returned values from functions go in $v0
 
 .text
+
 conv:
     # TODO: Write your function code here
+    # a0 = x, a1 = y, v0 = z, t0 = i
     li $v0, 0
-    li, $t0, 0
+    li, $t0, 8
 
 loop:
-    beq $t0, 8, done
-    mul $t1, $a0, 8
+    beq $t0, $zero, done
+    sll $t1, $a0, 3
     sub $v0, $v0, $t1
     add $v0, $v0, $a1
     slt $t2, $a0, 2
-    bne $t2, $zero, increment
+    bne $t2, $zero, loop
     sub $a1, $a1, 1
     addi $a0, $a0, 1
-
-increment:
-    addi $t0, $t0, 1
-    addi $a0, $a0, 1
+    addi $t0, $t0, -1
     j loop
 
 done:
     jr $ra
+
 
 main:  # DO NOT MODIFY THE MAIN SECTION
     li $a0, 5
